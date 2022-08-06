a8cb5e9c35f2d04e77afc272407935cb62533841
dovicie <hyokoyama8810@gmail.com>
2022-08-06 20:13:47 +0900

    💄

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,4 +13,18 @@ export default defineNuxtConfig({
       },
     },
   },
+  meta: {                      // !!Nuxt3からheadになったので注意！！！
+    htmlAttrs: {
+      lang: 'ja',
+      'data-theme': "cupcake"  // テーマを選択
+    },
+    title: 'Revium',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  }
 });
