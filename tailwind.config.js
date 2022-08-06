77e5334ec9b32a707a3447b256e0ee53cebfbe8f
dovicie <hyokoyama8810@gmail.com>
2022-08-06 18:22:02 +0900

    🎉TaiwindCSS+daisyUI 導入

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    './app.{vue,js,ts,jsx,tsx}',
+    './components/**/*.{vue,js,ts,jsx,tsx}',
+    './layouts/**/*.{vue,js,ts,jsx,tsx}',
+    './pages/**/*.{vue,js,ts,jsx,tsx}',
+    './plugins/**/*.{js,ts}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
