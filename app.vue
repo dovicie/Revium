ba9f5fe6654a03b4ff26a41d569b2f0120737d58
dovicie <hyokoyama8810@gmail.com>
2022-08-07 10:07:27 +0900

    ✨ Header 追加

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup>
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from './components/TheFooter.vue';
+</script>
+
 <template>
   <div>
+    <TheHeader />
     <NuxtPage />
+    <!-- <TheFooter /> -->
   </div>
 </template>
