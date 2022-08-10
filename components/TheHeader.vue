750a47492167bbdd55a2839edd7bb308965ac8cb
dovicie <hyokoyama8810@gmail.com>
2022-08-10 09:27:30 +0900

    💄 トップページ マークアップ+スタイリング

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,33 +1,15 @@
 <template>
-  <div class="navbar bg-base-100">
-    <div class="flex-1">
-      <a class="btn btn-ghost normal-case text-xl">Revium</a>
+  <header class="flex justify-between items-center p-2">
+    <h1 class="text-4xl font-bold text-primary">Revium</h1>
+    <div class="dropdown dropdown-end">
+      <label tabindex="0" class="btn btn-ghost m-1">三</label>
+      <ul
+        tabindex="0"
+        class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li><a>ホーム</a></li>
+        <li><a>このアプリについて</a></li>
+      </ul>
     </div>
-    <div class="flex-none">
-      <div class="dropdown dropdown-end">
-        <button class="btn btn-square btn-ghost" tabindex="0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
-        <ul
-          tabindex="0"
-          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li><a>ホーム</a></li>
-          <li><a>このアプリについて</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
+  </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,64 @@
 <template>
-  
+  <div class="p-2 flex flex-col gap-y-4">
+    <div>
+      <img
+        src="https://www.axis-corp.com/blog/wp-content/uploads/2016/01/ee6470d628534090ef9a7eca7858aa53.png"
+        alt=""
+        class="object-cover w-full"
+      />
+    </div>
+
+    <div>
+      <h2 class="h2">🗻 場所</h2>
+      <div class="flex flex-col sm:flex-row gap-y-2">
+        <input
+          type="text"
+          placeholder="新宿駅"
+          class="input input-bordered input-sm grow"
+        />
+        <div class="flex flex-row items-center justify-end gap-1">
+          <p class="shrink-0">から</p>
+          <select class="select select-bordered select-sm">
+            <option disabled selected>200m</option>
+            <option>500m</option>
+            <option>800m</option>
+            <option>1km</option>
+            <option>3km</option>
+            <option>5km</option>
+            <option>10km</option>
+          </select>
+          <p class="shrink-0">圏内</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h2 class="h2">🀄 キーワード</h2>
+      <input
+        type="text"
+        placeholder=""
+        class="input input-bordered w-full input-sm"
+      />
+    </div>
+    <div>
+      <h2 class="h2">🎯 ジャンル</h2>
+      <ul class="flex flex-wrap gap-2">
+        <li class="genre-btn">レストラン</li>
+        <li class="genre-btn">コーヒー</li>
+        <li class="genre-btn">観光スポット</li>
+        <li class="genre-btn">公園</li>
+        <li class="genre-btn">ラーメン</li>
+        <li class="genre-btn">動物園</li>
+        <li class="genre-btn">居酒屋</li>
+        <li class="genre-btn">ホテル</li>
+        <li class="genre-btn">バー</li>
+      </ul>
+    </div>
+    <div class="form-control">
+      <label class="label cursor-pointer justify-center gap-2">
+        <input type="checkbox" checked="checked" class="checkbox" />
+        <span class="label-text font-bold">営業中のスポットのみを表示</span>
+      </label>
+    </div>
+    <button class="btn btn-secondary">🔎 この条件で探す</button>
+  </div>
 </template>
