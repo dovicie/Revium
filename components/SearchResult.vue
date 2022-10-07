b0e1200791d5b55c17e80cb8574e2046c7503e63
dovicie <hyokoyama8810@gmail.com>
2022-10-07 09:11:36 +0900

    🚨 import computed

--- components/SearchResult.vue
+++ components/SearchResult.vue
@@ -1,11 +1,12 @@
 <script setup>
 import SearchResultItem from '~~/components/SearchResultItem.vue';
+import { computed } from 'vue';
 
 const props = defineProps({
 	places: Array,
 });
 
-const emit = defineEmits(['close']);
+defineEmits(['close']);
 
 const sortedPlaces = computed(() => {
 	return props.places.sort((a, b) => {
@@ -26,7 +27,7 @@ const sortedPlaces = computed(() => {
 			:types="place.types"
 		/>
 	</div>
-	<div class="h-16" />
+	<div class="h-16"></div>
 	<div
 		class="fixed bottom-0 left-0 py-2 w-full flex justify-center items-center gap-x-2 bg-white"
 	>
