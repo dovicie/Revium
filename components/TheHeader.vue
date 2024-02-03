ea255125050c8894e9821cb8bb0c0ba1d8ecb0f3
dovicie <hyokoyama8810@gmail.com>
2024-02-03 10:13:49 +0900

    💄

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="px-2 py-3 bg-white">
+  <header class="px-2 py-4 bg-white">
     <div class="flex justify-between items-center max-w-md w-full mx-auto">
-      <h1><img src="assets/logo.svg" alt="Revium" class="h-8" /></h1>
+      <h1><img src="assets/logo.svg" alt="Revium" class="h-6" /></h1>
     </div>
   </header>
 </template>
